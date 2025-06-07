eda76b9ecb99c38c409bfeb04a5024ea5ce249ea
KarolZubinski <zubinski.k@gmail.com>
2025-06-07 14:16:20 +0200

    reafactoring pulpit tests

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -7,12 +7,10 @@
             await page.getByTestId('password-input').fill('1wqr324r435');
             await page.getByTestId('login-button').click();
 
-            await page.goto('https://demo-bank.vercel.app/pulpit.html');
             await page.locator('#widget_1_transfer_receiver').selectOption('2');
-            await page.locator('#widget_1_transfer_amount').click();
             await page.locator('#widget_1_transfer_amount').fill('150');
-            await page.locator('#widget_1_transfer_title').click();
             await page.locator('#widget_1_transfer_title').fill('Zwrot');
+            
             await page.getByRole('button', { name: 'wykonaj' }).click();
             await page.getByTestId('close-button').click();
             await page.getByRole('link', { name: 'Przelew wykonany! Chuck' }).click();
