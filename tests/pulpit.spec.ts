e86b182e3a11919e28623e41301beaf741f092f6
KarolZubinski <zubinski.k@gmail.com>
2025-06-07 18:40:54 +0200

    add "domcontentloaded"

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,21 +1,23 @@
      import { test, expect } from '@playwright/test';
      
      test.describe('pulpit tests', () => {
-        test('quick payment with coorect data', async ({ page }) => {
+        test.only('quick payment with coorect data', async ({ page }) => {
             await page.goto('https://demo-bank.vercel.app/');
             await page.getByTestId('login-input').fill('testerLO');
             await page.getByTestId('password-input').fill('1wqr324r435');
             await page.getByTestId('login-button').click();
 
+            await page.waitForLoadState("domcontentloaded");
+
             await page.locator('#widget_1_transfer_receiver').selectOption('2');
             await page.locator('#widget_1_transfer_amount').fill('150');
-            await page.locator('#widget_1_transfer_title').fill('Zwrot');
+            await page.locator('#widget_1_transfer_title').fill('pizza');
 
             await page.getByRole('button', { name: 'wykonaj' }).click();
             await page.getByTestId('close-button').click();
             await page.getByRole('link', { name: 'Przelew wykonany! Chuck Demobankowy - 150,00 - Zwrot'});
 
-            await expect(page.locator("#show_messages")).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - Zwrot');
+            await expect(page.locator("#show_messages")).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza');
           });
 
      });
