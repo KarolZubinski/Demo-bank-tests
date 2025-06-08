2848fc898bced150a10baa3de33c05a59166fdb2
KarolZubinski <zubinski.k@gmail.com>
2025-06-08 19:40:46 +0200

    added assertion to mobile top-up test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('pulpit tests', () => {
-     test.only('quick payment with coorect data', async ({ page }) => {
+     test('quick payment with coorect data', async ({ page }) => {
           await page.goto('https://demo-bank.vercel.app/');
           await page.getByTestId('login-input').fill('testerLO');
           await page.getByTestId('password-input').fill('1wqr324r435');
@@ -20,19 +20,22 @@ test.describe('pulpit tests', () => {
           await expect(page.locator("#show_messages")).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza');
      });
 
-     test('Successful mobile top-up', async ({ page }) => {
+     test.only('Successful mobile top-up', async ({ page }) => {
           await page.goto('https://demo-bank.vercel.app/');
-          await page.getByTestId('login-input').click();
           await page.getByTestId('login-input').fill('testerLO');
-          await page.getByTestId('password-input').click();
           await page.getByTestId('password-input').fill('12345678');
           await page.getByTestId('login-button').click();
+
+          await page.waitForLoadState("domcontentloaded");
+
           await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
           await page.locator('#widget_1_topup_amount').click();
           await page.locator('#widget_1_topup_amount').fill('40');
           await page.locator('#uniform-widget_1_topup_agreement span').click();
           await page.getByRole('button', { name: 'doładuj telefon' }).click();
           await page.getByTestId('close-button').click();
-          await page.getByRole('link', { name: 'Doładowanie wykonane! 40,' }).click();
+          await page.getByRole('link', { name: 'Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx' });
+
+          await expect(page.locator("#show_messages")).toHaveText('Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx');
      });     
 });
