b9eb3c23934b9399073db83a3503f766ea80d2f3
KarolZubinski <zubinski.k@gmail.com>
2025-06-10 08:46:17 +0200

    refactoring pulpit tests

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -2,20 +2,29 @@ import { test, expect } from '@playwright/test';
 
 test.describe('pulpit tests', () => {
      test('quick payment with coorect data', async ({ page }) => {
-          await page.goto('https://demo-bank.vercel.app/');
-          await page.getByTestId('login-input').fill('testerLO');
-          await page.getByTestId('password-input').fill('1wqr324r435');
+          const url = "https://demo-bank.vercel.app/";
+          const userId = 'testerLO';
+          const userPassword = '1wqr324r435';
+
+          const receiverId = '2';
+          const transferAmount = '150';
+          const transferTitle = 'pizza';
+
+     
+          await page.goto(url);
+          await page.getByTestId('login-input').fill(userId);
+          await page.getByTestId('password-input').fill(userPassword);
           await page.getByTestId('login-button').click();
 
           await page.waitForLoadState("domcontentloaded");
 
-          await page.locator('#widget_1_transfer_receiver').selectOption('2');
-          await page.locator('#widget_1_transfer_amount').fill('150');
-          await page.locator('#widget_1_transfer_title').fill('pizza');
+          await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
+          await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+          await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
           await page.getByRole('button', { name: 'wykonaj' }).click();
           await page.getByTestId('close-button').click();
-          await page.getByRole('link', { name: 'Przelew wykonany! Chuck Demobankowy - 150,00 - Zwrot' });
+          await page.getByRole('link', { name: `Przelew wykonany! Chuck Demobankowy -${transferAmount},00 - ${transferTitle}`});
 
           await expect(page.locator("#show_messages")).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza');
      });
@@ -37,5 +46,5 @@ test.describe('pulpit tests', () => {
           await page.getByRole('link', { name: 'Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx' });
 
           await expect(page.locator("#show_messages")).toHaveText('Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx');
-     });     
+     });
 });
